2fd6e2392aaafb86c92557fbf401efdf647bd967
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-10-30 15:43:23 +0900

    初期データを追加 #2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      tasks: [
+        { id: 1, description: "task1", completed: false },
+        { id: 2, description: "task2", completed: false },
+        { id: 3, description: "task3", completed: true },
+        { id: 4, description: "task4", completed: true },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
